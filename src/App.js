cb4a57e247fe0eb50fab530300403591f724b4ab
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-04-09 08:26:46 -0400

    added footer and dictionary file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        Coded by <a href="https://www.linkedin.com/in/erin-buchanan-lmsw-ba2a2b160/">Erin Buchanan</a>
+      </footer>
+      </div>
     </div>
   );
 }

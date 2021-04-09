bd56beb9150739b648a83502f69ece37719a51c5
ebuchanan9 <79753303+ebuchanan9@users.noreply.github.com>
2021-04-09 19:48:46 -0400

    added CSS styling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <h1 className="text-center">Dictionary</h1>
+        <h1 className="heading">Dictionary</h1>
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword= "sunset"/>
       </main>
       <footer className="text-center">
         <small>Coded by <a href="https://www.linkedin.com/in/erin-buchanan-lmsw-ba2a2b160/" target="_blank" rel="noreferrer">Erin Buchanan</a></small>
